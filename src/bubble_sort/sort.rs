62c3facae0c510c1b9cde55963d0fe712c57dff7
Tanel <music.wave.from7@gmail.com>
2026-02-24 04:08:54 +0300

    input read

--- src/bubble_sort/sort.rs
+++ src/bubble_sort/sort.rs
@@ -1,3 +1,15 @@
+use std::io;
+
+
 fn main() {
-	print!("Please enter length of the array, you want to sort: ");		
+	println!("Please enter length of the array, you want to sort: ");	
+
+	let mut user_input = String::new();
+	io::stdin()
+	    .read_line(&mut user_input)
+	    .expect("Could nor read your data, i mean for real");
+
+	let x:i32  = user_input.trim().parse().expect("Input not an integer");
+
+	println! ("we willl be sorting array of langth:{}", x);
 }
